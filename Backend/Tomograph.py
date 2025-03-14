5dcb94f5f414b21f5cd5bf142b9a5ca64121c841
Miłosz <miloszsobolsci@gmail.com>
2025-03-14 13:20:20 +0100

    fixed getEmitterAndDetectorPoints, now it should give correct coordinates

--- Backend/Tomograph.py
+++ Backend/Tomograph.py
@@ -6,19 +6,21 @@ from matplotlib import pyplot as plt
 
 class Tomograph:
     def getEmitterAndDetectorPoints(self, angle: int, numberOfEmittersAndDetectors: int, angularSpread: int, radius: int, center: tuple):
-
-        def calculatePoints(angle: float, numberOfEmittersAndDetectors: int, angularSpread: float, radius: int, center: int):
+        def calculatePoints(angle: float, numberOfEmittersAndDetectors: int, angularSpread: float, radius: int, center: tuple):
             points = []
+            angle_step = angularSpread / (numberOfEmittersAndDetectors - 1) if numberOfEmittersAndDetectors > 1 else 0
+
             for i in range(numberOfEmittersAndDetectors):
-                x = int(radius * np.cos(angle + radians(angle * i)) - center[0])
-                y = int(radius * np.sin(angle + radians(angle * i)) - center[1])
+                current_angle = angle + angle_step * i
+                x = int(radius * np.cos(current_angle) + center[0])
+                y = int(radius * np.sin(current_angle) + center[1])
                 points.append((x, y))
+
             return np.array(points)
 
+        return (calculatePoints(radians(angle - angularSpread / 2), numberOfEmittersAndDetectors, radians(angularSpread), radius, center),
+            calculatePoints(radians(angle - angularSpread / 2 + 180), numberOfEmittersAndDetectors, radians(angularSpread), radius, center)[::-1])
 
-
-        return [calculatePoints(radians(angle - angularSpread/2), numberOfEmittersAndDetectors, radians(angularSpread), radius, center),
-            calculatePoints(radians(angle - angularSpread/2 + 180), numberOfEmittersAndDetectors, radians(angularSpread), radius, center)]
 
     def bresenham(self, x1, y1, x2, y2):
         pass
