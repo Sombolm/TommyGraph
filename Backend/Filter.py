48555cdc789b5daf8f04f5fd48f65301cc8457b8
Miłosz Sobol <miloszsobolsci@gmail.com>
2025-03-26 18:55:25 +0100

    refactored code

--- Backend/Filter.py
+++ Backend/Filter.py
@@ -4,14 +4,13 @@ from scipy.fftpack import fft, ifft, fftfreq
 
 class Filter:
 
-    #TODO: Implement this method
     def createFilter(self,size=21):
         k = np.arange(-size // 2, size // 2 + 1)
         kernel = np.zeros_like(k, dtype=np.float32)
 
         kernel[k == 0] = 1
-        odd_k = k[k % 2 != 0]
-        kernel[k % 2 != 0] = -4 / (np.pi ** 2 * odd_k ** 2)
+        oddK = k[k % 2 != 0]
+        kernel[k % 2 != 0] = -4 / (np.pi ** 2 * oddK ** 2)
         return kernel
 
     def filterSinogram(self, sinogram: np.ndarray, kernel) -> np.ndarray:
--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,8 @@
 from Backend.Tomograph import Tomograph
 
 tomograph = Tomograph()
-tomograph.run("../ExampleImages/Shepp_logan.jpg", 1, 180, 180, True, None,
-              False, dict(
+tomograph.run("../ExampleImages/Shepp_logan.jpg", 20, 180, 180, True, None,
+              True, dict(
     PatientName='Test',
     PatientID='001',
     ImageComments='Comment',
