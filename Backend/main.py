e95cc916e86650d3b3f98e4c712e1645aa905a63
phaast <aadam1712@interia.eu>
2025-04-11 18:22:20 +0200

    Update cleaned up some of the unused code

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,4 @@
 from Backend.Tomograph import Tomograph
 
 tomograph = Tomograph()
-tomograph.run("../ExampleImages/Kolo.jpg", 10, 180, 180, True, None,
-              False, dict(
-    PatientName='Test',
-    PatientID='001',
-    ImageComments='Comment',
-    StudyDate='20250321',
-), "../OutputImages/test.jpg")
+tomograph.run("../ExampleImages/Kolo.jpg", 10, 180, 180, True, None)
